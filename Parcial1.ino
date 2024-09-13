2c1bda4ce2c77356d46fcf7aec585b1879567c1a
adelaida19 <madelaida.angel@udea.edu.co>
2024-09-13 17:59:54 -0500

    Avance
    Incluye amplitud

--- Parcial1.ino
+++ Parcial1.ino
@@ -1,29 +1,106 @@
+#include <Adafruit_LiquidCrystal.h>
+Adafruit_LiquidCrystal lcd_1(0);
+
 #define led 2
 #define bpulsar 12
+#define bprocesar 13
+#define analogPin 0
 
-//const int led =2; // indica que el led está conectado al pin 2
-//const int bpulsar =12;
-int boton;  // // se declara la variable boton
+void agregar(int *arreglo, int num, int nDatos);
+void imprimir(int *arreglo, int n);
+void copiar(int *origen, int *destino, int nDatos);
+void amplitud(int *arreglo, int nDatos);
+
+int num=0;
+int nDatos=0;
+int *ptrAd= nullptr;
+int *aux = nullptr;
+bool adquirir = false;
+bool procesar = false;
+
+
+
 void setup() { 
   pinMode(bpulsar,INPUT); // se declara el pin 12 como entrada
-  pinMode(led, OUTPUT);
+  pinMode(bprocesar,INPUT);
   Serial.begin(9600);// se declara el pin 2 como salida
+  
+  lcd_1.begin(16, 2);
+  lcd_1.print("hello world");
 } 
+
 void loop() {
-  boton=digitalRead(bpulsar);  //se asigna a la variable “boton” el valor del pin 12
-  // // el led se enciende o no de acuerdo al valor del push button 
-  Serial.println(boton);
-  if(boton==LOW){
-  	digitalWrite(led,HIGH); 
-  }else{
-   digitalWrite(led,LOW); 
+  if(digitalRead(bpulsar)==LOW){
+    adquirir = true;
+  }else if(digitalRead(bprocesar)==LOW){
+  	adquirir = false;
+    imprimir(ptrAd,nDatos);
+    amplitud(ptrAd,nDatos);
+  }
+  
+  if(adquirir==true){
+    Serial.println("adquiriendo");
+    if(nDatos==0){
+      nDatos++;
+      ptrAd= new int[nDatos];
+      agregar(ptrAd,analogRead(analogPin), nDatos-1);
+
+    }else{
+      aux=new int[nDatos];
+      copiar(ptrAd,aux,nDatos);
+
+      delete[] ptrAd;
+      ptrAd=nullptr;
+
+      nDatos++;
+      ptrAd=new int[nDatos];
+      copiar(aux,ptrAd,nDatos-1);
+      agregar(ptrAd,analogRead(analogPin), nDatos-1);
+      delete[] aux;
+      aux=nullptr;
+    }
   }
 }
 
 
-//1. sensar los datos en un arreglo que este en un for, inicia
-//con el pulsador y para con el mismo pulasador
-//2. cuando inicies inicies el sensado invoca la funcion milis y guardas el tiempo en una variable
-//3. medir period
-//4. en el arreglo buscar el numero mayor y dividirlo por 100
 
+void agregar(int *arreglo, int num, int nDatos){
+    *(arreglo+nDatos)=num;
+}
+
+void imprimir(int *arreglo, int nDatos){
+   Serial.println("imprimiendo");  
+   if (nDatos==0) {
+        Serial.println("Aun no hay datos");
+    }
+    for (int i = 0; i < nDatos; ++i) {
+        Serial.println(*(arreglo+i));
+    }
+}
+
+void copiar(int *origen, int *destino, int nDatos){
+    for (int i = 0; i < nDatos; ++i) {
+        *(destino+i) = *(origen+i);
+    }
+}
+
+void amplitud(int *arreglo, int nDatos){
+	int amplitud=0;				
+  	for (int i = 0; i < nDatos; ++i) {
+      if(*(arreglo+i) < 0){
+        if((*(arreglo+i) * -1) > amplitud){
+        	amplitud = *(arreglo+i);
+          	amplitud = amplitud *(-1);
+        }
+      }else{
+      	if(*(arreglo+i) > amplitud){
+        	amplitud = *(arreglo+i);
+        }
+      }
+    }
+  	lcd_1.clear();
+  	lcd_1.print("Amplitud: ");
+  	lcd_1.setCursor(0, 1);
+  	lcd_1.print(amplitud/100);
+  	lcd_1.print(" V");
+}
