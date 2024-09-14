e6118f595c4e41a6ce777249252fd261e97efb0a
adelaida19 <madelaida.angel@udea.edu.co>
2024-09-13 20:03:15 -0500

    amplitud
    corrección de amplitud con offset

--- Parcial1.ino
+++ Parcial1.ino
@@ -6,16 +6,19 @@ Adafruit_LiquidCrystal lcd_1(0);
 #define bprocesar 13
 #define analogPin 0
 
+//tipo dato(void, int, float...) nombre()
 void agregar(int *arreglo, int num, int nDatos);
 void imprimir(int *arreglo, int n);
 void copiar(int *origen, int *destino, int nDatos);
+int offset(int *arreglo, int nDatos);
 void amplitud(int *arreglo, int nDatos);
 
-int num=0;
-int nDatos=0;
-int *ptrAd= nullptr;
-int *aux = nullptr;
-bool adquirir = false;
+int num=0; //guarda el valor del generador de señales
+int nDatos=0; //cantidad de datos en el heap
+int *ptrAd= nullptr; //puntero que almacena los nDatos
+int *aux = nullptr; //puntero que ayuda a cambiar el tamaño de datos
+
+bool adquirir = false; 
 bool procesar = false;
 
 
@@ -42,17 +45,17 @@ void loop() {
     Serial.println("adquiriendo");
     if(nDatos==0){
       nDatos++;
-      ptrAd= new int[nDatos];
+      ptrAd= new int[nDatos]; //creamos arreglo tipo enteros
       agregar(ptrAd,analogRead(analogPin), nDatos-1);
 
     }else{
       aux=new int[nDatos];
-      copiar(ptrAd,aux,nDatos);
+      copiar(ptrAd,aux,nDatos); //guardamos la información actual para soicitar mas espacio de memoria
 
-      delete[] ptrAd;
-      ptrAd=nullptr;
+      delete[] ptrAd; //borramos la memoria reservada actualmente 
+      ptrAd=nullptr; //hacemos que no sea un puntero peligroso
 
-      nDatos++;
+      nDatos++; //incrementamos la cantidad de datos para solicitar nueva memoria
       ptrAd=new int[nDatos];
       copiar(aux,ptrAd,nDatos-1);
       agregar(ptrAd,analogRead(analogPin), nDatos-1);
@@ -84,21 +87,58 @@ void copiar(int *origen, int *destino, int nDatos){
     }
 }
 
+int offset(int *arreglo, int nDatos){
+	int mayor=0;
+    int menor=0;
+  for(int i=0;i<nDatos; i++){
+    if (*(arreglo+i)> mayor){
+    	mayor=*(arreglo+i);
+    }
+    if (*(arreglo+i)<menor){
+    	menor=*(arreglo+i);
+    }
+  }
+  
+  Serial.println("mayor:");
+  Serial.println(mayor);
+  
+  Serial.println("menor:");
+  Serial.println(menor);
+  
+  int ff= menor+mayor;  
+  if (ff>-100 && ff<100){
+    return 0;
+  }
+  else {
+    Serial.println("offset:");
+  	Serial.println(ff/2);
+  	return (ff/2);
+  }
+  
+}
+
 void amplitud(int *arreglo, int nDatos){
-	int amplitud=0;				
+	float amplitud=0;				
   	for (int i = 0; i < nDatos; ++i) {
-      if(*(arreglo+i) < 0){
-        if((*(arreglo+i) * -1) > amplitud){
-        	amplitud = *(arreglo+i);
-          	amplitud = amplitud *(-1);
+      if(*(arreglo+i) < 0){ //pregunto si es negativo
+        if((*(arreglo+i) * -1) > amplitud){// si lo es lo comparo con amplitud y miro si es mayor
+        	amplitud = *(arreglo+i);// si es mayor, amplitud se actualiza con el valor del puntero
+          	amplitud = amplitud *(-1);// como era negativo lo hacemos positivo
         }
       }else{
-      	if(*(arreglo+i) > amplitud){
+      	if(*(arreglo+i) > amplitud){//pregunto si es positivo y mayor para actualizar amplitud
         	amplitud = *(arreglo+i);
         }
       }
     }
-  	lcd_1.clear();
+  	Serial.println("amplitud:");
+  	Serial.println(amplitud);
+  	int ff = 0;
+    ff = offset(arreglo, nDatos);
+  	amplitud=amplitud-ff;
+  
+  	lcd_1.clear(); //borra el display
+    lcd_1.setCursor(0, 0);
   	lcd_1.print("Amplitud: ");
   	lcd_1.setCursor(0, 1);
   	lcd_1.print(amplitud/100);
