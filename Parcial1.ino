16f75b571478093696f0522acbeac661ffee6bac
adelaida19 <madelaida.angel@udea.edu.co>
2024-09-15 00:03:32 -0500

    Frecuencia
    Incluye frecuencia e intento de identificacion de la señal

--- Parcial1.ino
+++ Parcial1.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_LiquidCrystal.h>
 Adafruit_LiquidCrystal lcd_1(0);
 
-#define led 2
 #define bpulsar 12
 #define bprocesar 13
 #define analogPin 0
@@ -13,6 +12,7 @@ void copiar(int *origen, int *destino, int nDatos);
 int offset(int *arreglo, int nDatos);
 void amplitud(int *arreglo, int nDatos);
 void frecuencia(float T);
+void forma(int *arreglo, int nDatos);
 
 int num=0; //guarda el valor del generador de señales
 int nDatos=0; //cantidad de datos en el heap
@@ -33,7 +33,7 @@ void setup() {
   Serial.begin(9600);// se declara el pin 2 como salida
   
   lcd_1.begin(16, 2);
-  lcd_1.print("hello world");
+  lcd_1.print("Parcial 1 Info2");
 } 
 
 void loop() {
@@ -51,6 +51,7 @@ void loop() {
     Serial.print("t1: ");
     Serial.println(t11);
     frecuencia((t22-t11)/1000);//
+    forma(ptrAd, nDatos);//
     
     delete[] ptrAd;//
     ptrAd=nullptr;//
@@ -65,9 +66,6 @@ void loop() {
       num=analogRead(analogPin);//
       agregar(ptrAd,num, nDatos-1);
       
-	  t1=millis();
-      Serial.println("t1:");
-  	  Serial.println(t1);
       if(num>0){//
         signo = true;
       }else{
@@ -75,17 +73,22 @@ void loop() {
       }
     }else{
       
-      if(num<0 && t<3){
+      if(num<0 && t<4){
         if(signo==true){
           t+=1;
           signo=false;
         }
       }
-      if(num>0 && t<3){
+      if(num>0 && t<4){
       	if(signo==false){
           t+=1;
           signo=true;
         }
+      }
+      if(t==1 && t1==0){
+        t1=millis();
+        Serial.println("t1:");
+        Serial.println(t1);
       }
       if(t==3 && t2==0){
       	t2=millis();
@@ -193,12 +196,99 @@ void amplitud(int *arreglo, int nDatos){
   	lcd_1.setCursor(0, 1);
   	lcd_1.print(amplitud/100);
   	lcd_1.print(" V");
+    delay(500);
 }
 void frecuencia(float T){//
 	lcd_1.clear();
     lcd_1.setCursor(0, 0);
   	lcd_1.print("frecuencia: ");
   	lcd_1.setCursor(0, 1);
-  	lcd_1.print(1/T);
+  	lcd_1.print((1/T));
   	lcd_1.print(" Hz");
+  	delay(500);
+}
+
+void forma(int *arreglo, int nDatos){
+	int s=0; //contadores s,t y c
+  	int t=0;
+  	int c=0;
+  	int v1=0; //amplitudes v1,v2
+  	int v2=0;
+  	int rango=0; // valor absoluto v2 - v1
+  	int tipo=0; //forma de la señal
+  for(int i=nDatos; i>=0 ; i--){
+    if((i-1)>=0){
+      v1=*(arreglo+(i-1));
+      if(v1 < 0){// si v1 y v2 son negativos los hacemos positivos
+      	v1= v1 * (-1);
+      }
+      v2=*(arreglo+i);
+      if(v2 < 0){
+      	v2= v2 * (-1);
+      }
+      rango=v2-v1;
+      Serial.println("rango: ");
+      Serial.println(rango);
+      if(rango < 0){
+      	rango=rango * (-1);//si el rango es negativo se hace positivo
+      }
+      if(rango <60 && rango!=0){//clasificación de la señal
+      	t+=1;
+      }
+      if(rango>70){
+      	s+=1;
+      }
+      if(rango==0){
+      	c+=1;
+      }
+    }
+  }
+  Serial.println("s: ");
+  Serial.println(s);
+  Serial.println("t: ");
+  Serial.println(t);
+  Serial.println("c: ");
+  Serial.println(c);
+  if(s > tipo){
+    tipo=s;
+  }
+  if(t > tipo){
+  	tipo=t;
+  }
+  if(c > tipo){
+  	tipo=c;
+  }
+  if(s<10 && t<10 && c<10){//señal desconocida
+  	tipo=0;
+  }
+  
+  if(tipo==s){
+  	lcd_1.clear();
+    lcd_1.setCursor(0, 0);
+  	lcd_1.print("tipo: ");
+  	lcd_1.setCursor(0, 1);
+  	lcd_1.print("Senoidal");
+  	delay(500);
+  }else if(tipo==t){
+    lcd_1.clear();
+    lcd_1.setCursor(0, 0);
+  	lcd_1.print("tipo: ");
+  	lcd_1.setCursor(0, 1);
+  	lcd_1.print("Triangular");
+  	delay(500);
+  }else if(tipo==c){
+    lcd_1.clear();
+    lcd_1.setCursor(0, 0);
+  	lcd_1.print("tipo: ");
+  	lcd_1.setCursor(0, 1);
+  	lcd_1.print("Cuadrada");
+  	delay(500);
+  }else if(tipo==0){
+  	lcd_1.clear();
+    lcd_1.setCursor(0, 0);
+  	lcd_1.print("tipo: ");
+  	lcd_1.setCursor(0, 1);
+    lcd_1.print("Desconocida");
+  	delay(500);
+  }
 }
